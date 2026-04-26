201d8032b37ddd5f42006ea7c2dc38e7a2c55dd4
Levi Lucas <levilucas93@gmail.com>
2026-04-26 07:32:27 +0100

    added in missing material

--- database/seeders/AttributesMaterialSeeder.php
+++ database/seeders/AttributesMaterialSeeder.php
@@ -24,6 +24,7 @@ class AttributesMaterialSeeder extends Seeder
             'Composite',
             'Steel',
             'Upvc',
+            'Wooden',
         ];
 
         foreach ($names as $name) {
