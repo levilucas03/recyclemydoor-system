bbb8d305c54ca0549bb7397ae44b824a3797b261
Levi Lucas <levilucas93@gmail.com>
2026-09-19 23:08:33 +0100

    fixed image issue

--- resources/js/pages/Sales/Index.vue
+++ resources/js/pages/Sales/Index.vue
@@ -1040,7 +1040,7 @@ function statusClasses(status) {
 
                                                             <img
                                                                 v-if="productImage(item.product)"
-                                                                :src="productImage(item.product)"
+                                                                :src="'/storage/' + productImage(item.product)"
                                                                 :alt="item.product?.title"
                                                                 class="h-14 w-14 rounded-md border border-gray-200 object-cover"
                                                             >
